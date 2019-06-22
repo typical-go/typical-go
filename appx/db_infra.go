f5b9117e9cbb5f23bdadd7860d111e1650020b19
iman tung <iman.tung@gmail.com>
2019-06-22 15:22:00 +0700

    change dbinfra migrate and rollback parameter

--- appx/db_infra.go
+++ appx/db_infra.go
@@ -1,11 +1,9 @@
 package appx
 
-import "gopkg.in/urfave/cli.v1"
-
 // DBInfra database infrastructure
 type DBInfra interface {
 	Create() (err error)
 	Drop() (err error)
-	Migrate(args cli.Args) error
-	Rollback(args cli.Args) error
+	Migrate(source string) error
+	Rollback(source string) error
 }
