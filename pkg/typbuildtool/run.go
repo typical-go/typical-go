a6202e5189fcc5f04f41e2ab5176fbcad992768b
iman tung <iman.tung@gmail.com>
2019-11-27 06:08:54 +0700

    skip flag parsing in run command and add standard input

--- pkg/typbuildtool/run.go
+++ pkg/typbuildtool/run.go
@@ -1,33 +1,33 @@
 package typbuildtool
 
 import (
+	"os"
+	"os/exec"
+
 	log "github.com/sirupsen/logrus"
 	"github.com/typical-go/typical-go/pkg/typenv"
-	"github.com/typical-go/typical-go/pkg/utility/bash"
 	"github.com/urfave/cli"
 )
 
 func (t buildtool) cmdRun() cli.Command {
 	return cli.Command{
-		Name:      "run",
-		ShortName: "r",
-		Usage:     "Run the binary",
-		Flags: []cli.Flag{
-			cli.BoolFlag{
-				Name:  "no-build",
-				Usage: "Run the binary without build",
-			},
-		},
-		Action: t.runBinary,
+		Name:            "run",
+		ShortName:       "r",
+		Usage:           "Run the binary",
+		SkipFlagParsing: true,
+		Action:          t.runBinary,
 	}
 }
 
 func (t buildtool) runBinary(ctx *cli.Context) (err error) {
-	if !ctx.Bool("no-build") {
-		if err = t.buildBinary(ctx); err != nil {
-			return
-		}
+	if err = t.buildBinary(ctx); err != nil {
+		return
 	}
 	log.Info("Run the application")
-	return bash.Run(typenv.App.BinPath, []string(ctx.Args())...)
+	args := []string(ctx.Args())
+	cmd := exec.Command(typenv.App.BinPath, args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stdout
+	cmd.Stdin = os.Stdin
+	return cmd.Run()
 }
