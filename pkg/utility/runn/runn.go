c88e8b99dee13b36aabf18b0fc85bc07ede5d538
iman tung <iman.tung@gmail.com>
2019-11-26 11:03:55 +0700

    remove executor as not needed

--- pkg/utility/runn/runn.go
+++ pkg/utility/runn/runn.go
@@ -1,6 +1,21 @@
 package runn
 
-// Execute statements as stop when error
+import (
+	"fmt"
+	"reflect"
+)
+
+// Execute all statement
 func Execute(stmts ...interface{}) error {
-	return Executor{StopWhenError: true}.Execute(stmts...)
+	for i, stmt := range stmts {
+		switch stmt.(type) {
+		case Runner:
+			return stmt.(Runner).Run()
+		case func() error:
+			return stmt.(func() error)()
+		default:
+			return fmt.Errorf("Statement-%d: Invalid: %s", i, reflect.TypeOf(stmt))
+		}
+	}
+	return nil
 }
--- pkg/utility/runn/runn_test.go
+++ pkg/utility/runn/runn_test.go
@@ -1,6 +1,8 @@
 package runn_test
 
 import (
+	"errors"
+	"os/exec"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -8,10 +10,61 @@ import (
 )
 
 func TestExecute(t *testing.T) {
-	err := runn.Execute(
-		runner{"some-error-2"},
-		runner{"some-error-1"},
-	)
-	require.EqualError(t, err, "some-error-2")
+	testcases := []struct {
+		stmts  []interface{}
+		errMsg string
+	}{
+		{
+			stmts: []interface{}{
+				runner{"some-error"},
+				exec.Command("wrong-command", "bad-argument"),
+			},
+			errMsg: "some-error",
+		},
+		{
+			stmts: []interface{}{
+				errors.New("error1"),
+				errors.New("error2"),
+			},
+			errMsg: "Statement-0: Invalid: *errors.errorString",
+		},
+		{
+			stmts: []interface{}{
+				runner{"some-error-1"},
+				runner{"some-error-2"},
+			},
+			errMsg: "some-error-1",
+		},
+		{
+			stmts: []interface{}{
+				func() error { return errors.New("some-error-1") },
+				func() error { return errors.New("some-error-2") },
+			},
+			errMsg: "some-error-1",
+		},
+		{
+			stmts:  []interface{}{},
+			errMsg: "",
+		},
+	}
 
+	for i, tt := range testcases {
+		err := runn.Execute(tt.stmts...)
+		if tt.errMsg == "" {
+			require.NoError(t, err)
+		} else {
+			require.EqualError(t, err, tt.errMsg, i)
+		}
+	}
+}
+
+type runner struct {
+	msg string
+}
+
+func (r runner) Run() error {
+	if r.msg == "" {
+		return nil
+	}
+	return errors.New(r.msg)
 }
