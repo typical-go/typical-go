0e6c188ca0dcf51bf5501b9c32394b40b7aba11d
iman tung <iman.tung@gmail.com>
2019-12-27 21:56:29 +0700

    constructor for app module

--- app/module.go
+++ app/module.go
@@ -10,35 +10,17 @@ const (
 	Version = "0.9.21"
 )
 
-// Module of app
-type Module struct{}
+// Module of Typical-Go
+func Module() interface{} {
+	return &module{}
+}
 
-// AppCommands is commands collection to execute application
-func (m Module) AppCommands(c *typcore.Context) []*cli.Command {
+type module struct{}
+
+func (m module) AppCommands(c *typcore.Context) []*cli.Command {
 	return []*cli.Command{
 		cmdConstructProject(),
 		cmdConstructModule(),
 		cmdCreateWrapper(),
 	}
 }
-
-// -- Uncomment to test action --
-// func (Module) Action() interface{} {
-// 	return func(cfg Config) {
-// 		fmt.Printf("Hello %s\n", cfg.Hello)
-// 	}
-// }
-
-// func (Module) Configure() (prefix string, spec, loadFn interface{}) {
-// 	prefix = "APP"
-// 	spec = &Config{}
-// 	loadFn = func(loader typcore.ConfigLoader) (cfg Config, err error) {
-// 		err = loader.Load(prefix, &cfg)
-// 		return
-// 	}
-// 	return
-// }
-
-// type Config struct {
-// 	Hello string `default:"world"`
-// }
--- typical/descriptor.go
+++ typical/descriptor.go
@@ -11,11 +11,9 @@ var Descriptor = &typcore.ProjectDescriptor{
 	Name:      "Typical-Go",
 	Version:   app.Version,
 	Package:   "github.com/typical-go/typical-go",
-	AppModule: &app.Module{},
-	Releaser: typrls.New(
-		"linux/amd64",
-		"darwin/amd64",
-	).WithPublisher(
-		typrls.GithubPublisher("typical-go", "typical-go"),
-	),
+	AppModule: app.Module(),
+	Releaser: typrls.New("linux/amd64", "darwin/amd64").
+		WithPublisher(
+			typrls.GithubPublisher("typical-go", "typical-go"),
+		),
 }
