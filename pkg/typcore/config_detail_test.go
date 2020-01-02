d8a91e3169f14440049a553410b4b7cfa9dad815
iman tung <iman.tung@gmail.com>
2020-01-02 21:41:07 +0700

    revision: fix test for config details

--- pkg/typcore/config_detail_test.go
+++ pkg/typcore/config_detail_test.go
@@ -7,21 +7,18 @@ import (
 	"github.com/typical-go/typical-go/pkg/typcore"
 )
 
-func TestField(t *testing.T) {
-	names, configMap := typcore.ConfigMap("TEST", &sampleSpec{
+func TestCreateConfigDetails(t *testing.T) {
+	spec := struct {
+		Field1 string `default:"hello" required:"true"`
+		Field2 string `default:"world"`
+		Field3 string `ignored:"true"`
+		Field4 int    `envconfig:"ALIAS"`
+	}{
 		Field2: "mama",
-	})
-	require.Equal(t, []string{"TEST_FIELD1", "TEST_FIELD2", "TEST_ALIAS"}, names)
-	require.EqualValues(t, map[string]typcore.ConfigDetail{
-		"TEST_FIELD1": {Name: "TEST_FIELD1", Type: "string", Default: "hello", Value: "", IsZero: true, Required: true},
-		"TEST_FIELD2": {Name: "TEST_FIELD2", Type: "string", Default: "world", Value: "mama", IsZero: false, Required: false},
-		"TEST_ALIAS":  {Name: "TEST_ALIAS", Type: "int", Default: "", Value: 0, IsZero: true, Required: false},
-	}, configMap)
-}
-
-type sampleSpec struct {
-	Field1 string `default:"hello" required:"true"`
-	Field2 string `default:"world"`
-	Field3 string `ignored:"true"`
-	Field4 int    `envconfig:"ALIAS"`
+	}
+	require.EqualValues(t, []typcore.ConfigDetail{
+		{Name: "TEST_FIELD1", Type: "string", Default: "hello", Value: "", IsZero: true, Required: true},
+		{Name: "TEST_FIELD2", Type: "string", Default: "world", Value: "mama", IsZero: false, Required: false},
+		{Name: "TEST_ALIAS", Type: "int", Default: "", Value: 0, IsZero: true, Required: false},
+	}, typcore.CreateConfigDetails("TEST", &spec))
 }
