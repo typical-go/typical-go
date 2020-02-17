a99d20fefb2f7ce395d1614a059968b818becaf5
iman tung <iman.tung@gmail.com>
2020-02-17 10:53:50 +0700

    remove descriptor interface as it is dispensible

--- pkg/typcore/run.go
+++ pkg/typcore/run.go
@@ -4,24 +4,16 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-type appDescriptor interface {
-	RunApp() error
-}
-
-type buildDescriptor interface {
-	RunBuild() error
-}
-
 // RunApp the application
-func RunApp(d appDescriptor) {
-	if err := d.RunApp(); err != nil {
+func RunApp(i interface{ RunApp() error }) {
+	if err := i.RunApp(); err != nil {
 		log.Fatal(err.Error())
 	}
 }
 
 // RunBuildTool the build tool
-func RunBuildTool(d buildDescriptor) {
-	if err := d.RunBuild(); err != nil {
+func RunBuildTool(i interface{ RunBuild() error }) {
+	if err := i.RunBuild(); err != nil {
 		log.Fatal(err.Error())
 	}
 }
