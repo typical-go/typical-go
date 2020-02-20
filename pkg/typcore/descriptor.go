dabb05395fb715f0dedc1b43a4d6dbbd1ed4f956
iman tung <iman.tung@gmail.com>
2020-02-20 10:51:17 +0700

    godoc for descriptor attributes

--- pkg/typcore/descriptor.go
+++ pkg/typcore/descriptor.go
@@ -7,14 +7,35 @@ import (
 
 // Descriptor describe the project
 type Descriptor struct {
-	Name        string
-	Description string
-	Package     string
-	Version     string
-	Sources     []string
 
-	App           App
-	BuildTool     BuildTool
+	// Name of the project (OPTIONAL)
+	// It should be a characters with/without underscore or dash.
+	// By default, project name is same with project folder
+	Name string
+
+	// Description of the project (OPTIONAL)
+	Description string
+
+	// Version of the project (MANDATORY)
+	Version string
+
+	// Package of the project (MANDATORY)
+	// It should be same with go.mod file
+	Package string
+
+	// Sources of project (OPTIONAL)
+	// If `App` is sourceable, the sources will be take from it.
+	// Else, by default, sources will be package name of `App` type
+	// If `pkg` folder available, it will be added to sources.
+	Sources []string
+
+	// App of the project (MANDATORY)
+	App App
+
+	// BuildTool of the project (MANDATORY)
+	BuildTool BuildTool
+
+	// Configuration of the project (OPTIONAL)
 	Configuration Configuration
 }
 
