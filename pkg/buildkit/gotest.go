285f07019d4755060c9ef10539eeb0cee9529610
iman tung <iman.tung@gmail.com>
2020-03-26 19:43:57 +0700

    add timeout in gotest and validate if no test target

--- pkg/buildkit/gotest.go
+++ pkg/buildkit/gotest.go
@@ -2,37 +2,51 @@ package buildkit
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"io"
+	"os"
 	"os/exec"
+	"time"
 )
 
 // GoTest builder
 type GoTest struct {
-	CoverProfile string
-	Race         bool
-	Targets      []string
-	stdout       io.Writer
-	stderr       io.Writer
-	stdin        io.Reader
+	targets      []string
+	coverProfile string
+	race         bool
+	timeout      time.Duration
+
+	stdout io.Writer
+	stderr io.Writer
+	stdin  io.Reader
 }
 
 // NewGoTest return new instance of GoTest
 func NewGoTest(targets ...string) *GoTest {
 	return &GoTest{
-		Targets: targets,
+		targets: targets,
+		timeout: 20 * time.Second,
+		stdout:  os.Stdout,
+		stderr:  os.Stderr,
 	}
+}
+
+// WithTimeout return GotTest with new timeout
+func (g *GoTest) WithTimeout(timeout time.Duration) *GoTest {
+	g.timeout = timeout
+	return g
 }
 
 // WithRace return GoTest with new race
 func (g *GoTest) WithRace(race bool) *GoTest {
-	g.Race = race
+	g.race = race
 	return g
 }
 
 // WithCoverProfile return GoTest with new cover profile
 func (g *GoTest) WithCoverProfile(coverProfile string) *GoTest {
-	g.CoverProfile = coverProfile
+	g.coverProfile = coverProfile
 	return g
 }
 
@@ -56,6 +70,9 @@ func (g *GoTest) WithStdin(stdin io.Reader) *GoTest {
 
 // Execute comand
 func (g *GoTest) Execute(ctx context.Context) (err error) {
+	if len(g.targets) < 1 {
+		return errors.New("Nothing to test")
+	}
 	cmd := exec.CommandContext(ctx, "go", g.Args()...)
 	cmd.Stdout = g.stdout
 	cmd.Stderr = g.stderr
@@ -66,12 +83,18 @@ func (g *GoTest) Execute(ctx context.Context) (err error) {
 // Args is argument for gotest
 func (g *GoTest) Args() []string {
 	args := []string{"test"}
-	if g.CoverProfile != "" {
-		args = append(args, fmt.Sprintf("-coverprofile=%s", g.CoverProfile))
+
+	if g.timeout > 0 {
+		args = append(args, fmt.Sprintf("-timeout=%s", g.timeout.String()))
 	}
-	if g.Race {
+
+	if g.coverProfile != "" {
+		args = append(args, fmt.Sprintf("-coverprofile=%s", g.coverProfile))
+	}
+
+	if g.race {
 		args = append(args, "-race")
 	}
-	args = append(args, g.Targets...)
-	return args
+
+	return append(args, g.targets...)
 }
--- pkg/buildkit/gotest_test.go
+++ pkg/buildkit/gotest_test.go
@@ -1,7 +1,9 @@
 package buildkit_test
 
 import (
+	"context"
 	"testing"
+	"time"
 
 	"github.com/typical-go/typical-go/pkg/buildkit"
 
@@ -15,16 +17,27 @@ func TestGotTest_Args(t *testing.T) {
 	}{
 		{
 			GoTest:   buildkit.NewGoTest("target1", "target2"),
-			expected: []string{"test", "target1", "target2"},
+			expected: []string{"test", "-timeout=20s", "target1", "target2"},
 		},
 		{
 			GoTest: buildkit.NewGoTest("target1", "target2").
-				WithRace(true).WithCoverProfile("some-coverprofile"),
-			expected: []string{"test", "-coverprofile=some-coverprofile", "-race", "target1", "target2"},
+				WithTimeout(10 * time.Second).
+				WithRace(true).
+				WithCoverProfile("some-coverprofile"),
+			expected: []string{"test", "-timeout=10s", "-coverprofile=some-coverprofile", "-race", "target1", "target2"},
 		},
 	}
 
 	for _, tt := range testcases {
 		require.Equal(t, tt.expected, tt.Args())
 	}
+}
+
+func TestGoTest_Execute(t *testing.T) {
+	t.Run("GIVEN no test targets", func(t *testing.T) {
+		require.EqualError(t,
+			buildkit.NewGoTest().Execute(context.Background()),
+			"Nothing to test",
+		)
+	})
 }
