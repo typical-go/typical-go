1faf137d5b7d766079f138805a94a89bdaa01d84
iman tung <iman.tung@gmail.com>
2020-03-27 19:23:03 +0700

    add SimpleCommand as simple implementation of tyapp.Commander

---
+++ pkg/typapp/simple_commander.go
@@ -0,0 +1,26 @@
+package typapp
+
+import "github.com/urfave/cli/v2"
+
+// SimpleCommander is simple implementation of Commander
+type SimpleCommander struct {
+	fns []CommandFn
+}
+
+// CommandFn is a function to return command
+type CommandFn func(ctx *Context) []*cli.Command
+
+// NewCommander return new instance of SimpleComander
+func NewCommander(fns ...CommandFn) *SimpleCommander {
+	return &SimpleCommander{
+		fns: fns,
+	}
+}
+
+// Commands of application
+func (s *SimpleCommander) Commands(c *Context) (cmds []*cli.Command) {
+	for _, fn := range s.fns {
+		cmds = append(cmds, fn(c)...)
+	}
+	return
+}
