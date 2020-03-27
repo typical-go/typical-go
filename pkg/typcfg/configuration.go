6416422a01fc128f50fb860674b8a9585aa7d0c7
iman tung <iman.tung@gmail.com>
2020-03-27 11:11:18 +0700

    add with's function to modify configuration attributes

--- pkg/typcfg/configuration.go
+++ pkg/typcfg/configuration.go
@@ -17,6 +17,18 @@ func NewConfiguration(name string, spec interface{}) *Configuration {
 	}
 }
 
+// WithName return Configuration with new name
+func (c *Configuration) WithName(name string) *Configuration {
+	c.Name = name
+	return c
+}
+
+// WithSpec return Configuration with new spec
+func (c *Configuration) WithSpec(spec interface{}) *Configuration {
+	c.Spec = spec
+	return c
+}
+
 // Configure return the configuration
 func (c *Configuration) Configure() *Configuration {
 	return c
--- pkg/typcfg/configuration_test.go
+++ pkg/typcfg/configuration_test.go
@@ -10,12 +10,24 @@ import (
 
 func TestConfiguration(t *testing.T) {
 	t.Run("SHOULD implement typcfg.Configurer", func(t *testing.T) {
-		var _ typcfg.Configurer = typcfg.NewConfiguration("some-name", "some-spec")
+		var _ typcfg.Configurer = &typcfg.Configuration{}
 	})
+}
 
+func TestConfiguration_Constructor(t *testing.T) {
 	t.Run("", func(t *testing.T) {
 		cfg := typcfg.NewConfiguration("some-name", "some-spec")
 		require.Equal(t, "some-name", cfg.Name)
 		require.Equal(t, "some-spec", cfg.Spec)
+	})
+
+	t.Run("with's functions", func(t *testing.T) {
+		cfg := typcfg.
+			NewConfiguration("name", "spec").
+			WithName("new-name").
+			WithSpec("new-spec")
+
+		require.Equal(t, "new-name", cfg.Name)
+		require.Equal(t, "new-spec", cfg.Spec)
 	})
 }
