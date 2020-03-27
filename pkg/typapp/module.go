aa8c575dc85ddd61b6406587524be2037d21d305
iman tung <iman.tung@gmail.com>
2020-03-27 22:37:26 +0700

    app module support multiple entity

--- pkg/typapp/module.go
+++ pkg/typapp/module.go
@@ -6,40 +6,69 @@ import (
 
 // Module for application
 type Module struct {
-	Provider  Provider
-	Destroyer Destroyer
-	Preparer  Preparer
-	Commander Commander
+	providers  []Provider
+	destroyers []Destroyer
+	preparers  []Preparer
+	commanders []Commander
+}
+
+// NewModule return new instance of Module
+func NewModule() *Module {
+	return &Module{}
+}
+
+// WithProviders return Module with new providers
+func (m *Module) WithProviders(providers ...Provider) *Module {
+	m.providers = providers
+	return m
+}
+
+// WithDestoyers return Module with new destroyers
+func (m *Module) WithDestoyers(destroyers ...Destroyer) *Module {
+	m.destroyers = destroyers
+	return m
+}
+
+// WithPrepares return Module with new preparers
+func (m *Module) WithPrepares(prepares ...Preparer) *Module {
+	m.preparers = prepares
+	return m
+}
+
+// WithCommanders return Module with new commanders
+func (m *Module) WithCommanders(commanders ...Commander) *Module {
+	m.commanders = commanders
+	return m
 }
 
 // Provide dependency
-func (m *Module) Provide() []*Constructor {
-	if m.Provider != nil {
-		return m.Provider.Provide()
+func (m *Module) Provide() (constructions []*Constructor) {
+	for _, provider := range m.providers {
+		constructions = append(constructions, provider.Provide()...)
 	}
-	return []*Constructor{}
+	return
 }
 
 // Destroy dependency
-func (m *Module) Destroy() []*Destruction {
-	if m.Destroyer != nil {
-		return m.Destroyer.Destroy()
+func (m *Module) Destroy() (destructions []*Destruction) {
+	for _, destroyer := range m.destroyers {
+		destructions = append(destructions, destroyer.Destroy()...)
 	}
-	return []*Destruction{}
+	return
 }
 
 // Prepare dependency
-func (m *Module) Prepare() []*Preparation {
-	if m.Preparer != nil {
-		return m.Preparer.Prepare()
+func (m *Module) Prepare() (preparations []*Preparation) {
+	for _, prepare := range m.preparers {
+		preparations = append(preparations, prepare.Prepare()...)
 	}
-	return []*Preparation{}
+	return
 }
 
 // Commands of application
-func (m *Module) Commands(c *Context) []*cli.Command {
-	if m.Commander != nil {
-		return m.Commander.Commands(c)
+func (m *Module) Commands(c *Context) (cmds []*cli.Command) {
+	for _, commander := range m.commanders {
+		cmds = append(cmds, commander.Commands(c)...)
 	}
-	return []*cli.Command{}
+	return
 }
--- pkg/typapp/module_test.go
+++ pkg/typapp/module_test.go
@@ -21,30 +21,25 @@ func TestModule(t *testing.T) {
 	t.Run("SHOULD implement commander", func(t *testing.T) {
 		var _ typapp.Commander = &typapp.Module{}
 	})
-	t.Run("GIVEN empty attribute", func(t *testing.T) {
-		mod := &typapp.Module{}
-		require.Equal(t, []*cli.Command{}, mod.Commands(nil))
-		require.Equal(t, []*typapp.Preparation{}, mod.Prepare())
-		require.Equal(t, []*typapp.Constructor{}, mod.Provide())
-		require.Equal(t, []*typapp.Destruction{}, mod.Destroy())
-	})
-	t.Run("GIVE some attribute", func(t *testing.T) {
-		cmd := &cli.Command{}
-		preparation := typapp.NewPreparation(nil)
-		constructor := typapp.NewConstructor(nil)
-		destruction := typapp.NewDestruction(nil)
 
-		mod := &typapp.Module{
-			Commander: typapp.NewCommander(func(*typapp.Context) []*cli.Command {
-				return []*cli.Command{cmd}
-			}),
-			Preparer:  preparation,
-			Provider:  constructor,
-			Destroyer: destruction,
-		}
-		require.Equal(t, []*cli.Command{cmd}, mod.Commands(nil))
-		require.Equal(t, []*typapp.Preparation{preparation}, mod.Prepare())
-		require.Equal(t, []*typapp.Constructor{constructor}, mod.Provide())
-		require.Equal(t, []*typapp.Destruction{destruction}, mod.Destroy())
-	})
+}
+
+func TestNewModule(t *testing.T) {
+	cmd := &cli.Command{}
+	preparation := typapp.NewPreparation(nil)
+	constructor := typapp.NewConstructor(nil)
+	destruction := typapp.NewDestruction(nil)
+
+	mod := typapp.NewModule().
+		WithCommanders(typapp.NewCommander(func(*typapp.Context) []*cli.Command {
+			return []*cli.Command{cmd}
+		})).
+		WithPrepares(preparation).
+		WithDestoyers(destruction).
+		WithProviders(constructor)
+
+	require.Equal(t, []*cli.Command{cmd}, mod.Commands(nil))
+	require.Equal(t, []*typapp.Preparation{preparation}, mod.Prepare())
+	require.Equal(t, []*typapp.Constructor{constructor}, mod.Provide())
+	require.Equal(t, []*typapp.Destruction{destruction}, mod.Destroy())
 }
