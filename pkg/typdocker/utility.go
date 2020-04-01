7010b1a663dd0e22d9cc4c3f66dca15a67cea935
iman tung <iman.tung@gmail.com>
2020-04-01 18:59:43 +0700

    add -wipe flag when docker up to clear all docker container in local machine + tidy up

--- pkg/typdocker/utility.go
+++ pkg/typdocker/utility.go
@@ -13,6 +13,10 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
+const (
+	dockerComposeFile = "docker-compose.yml"
+)
+
 var (
 	_ typbuildtool.Utility = (*DockerUtility)(nil)
 )
@@ -47,63 +51,43 @@ func (m *DockerUtility) Commands(c *typbuildtool.Context) []*cli.Command {
 				{
 					Name:  "compose",
 					Usage: "Generate docker-compose.yaml",
-					Action: func(cliCtx *cli.Context) (err error) {
-						if len(m.composers) < 1 {
-							return errors.New("Nothing to compose")
-						}
-						var out []byte
+					Action: func(cc *cli.Context) (err error) {
 						c.Info("Generate docker-compose.yml")
-						if out, err = yaml.Marshal(m.dockerCompose()); err != nil {
-							return
-						}
-						if err = ioutil.WriteFile("docker-compose.yml", out, 0644); err != nil {
-							return
-						}
-						return
+						return m.compose()
 					},
 				},
 				{
-					Name:  "up",
+					Name:    "up",
+					Aliases: []string{"start"},
+					Flags: []cli.Flag{
+						&cli.BoolFlag{Name: "wipe"},
+					},
 					Usage: "Spin up docker containers according docker-compose",
-					Action: func(ctx *cli.Context) (err error) {
-						cmd := exec.Command("docker-compose", "up", "--remove-orphans", "-d")
-						cmd.Stderr = os.Stderr
-						cmd.Stdout = os.Stdout
-						return cmd.Run()
+					Action: func(cc *cli.Context) (err error) {
+						bc := c.BuildContext(cc)
+						if cc.Bool("wipe") {
+							c.Info("Wipe all docker container")
+							m.wipe(bc)
+						}
+						c.Info("Docker up")
+						return m.up(bc)
 					},
 				},
 				{
-					Name:  "down",
-					Usage: "Take down all docker containers according docker-compose",
-					Action: func(ctx *cli.Context) (err error) {
-						cmd := exec.Command("docker-compose", "down")
-						cmd.Stderr = os.Stderr
-						cmd.Stdout = os.Stdout
-						return cmd.Run()
+					Name:    "down",
+					Aliases: []string{"stop"},
+					Usage:   "Take down all docker containers according docker-compose",
+					Action: func(cc *cli.Context) (err error) {
+						c.Info("Docker down")
+						return m.down(c.BuildContext(cc))
 					},
 				},
 				{
 					Name:  "wipe",
 					Usage: "Kill all running docker container",
-					Action: func(ctx *cli.Context) (err error) {
-						var builder strings.Builder
-						cmd := exec.Command("docker", "ps", "-q")
-						cmd.Stderr = os.Stderr
-						cmd.Stdout = &builder
-						if err = cmd.Run(); err != nil {
-							return
-						}
-						dockerIDs := strings.Split(builder.String(), "\n")
-						for _, id := range dockerIDs {
-							if id != "" {
-								cmd := exec.Command("docker", "kill", id)
-								cmd.Stderr = os.Stderr
-								if err = cmd.Run(); err != nil {
-									return
-								}
-							}
-						}
-						return
+					Action: func(cc *cli.Context) (err error) {
+						c.Info("Wipe all docker container")
+						return m.wipe(c.BuildContext(cc))
 					},
 				},
 			},
@@ -111,7 +95,59 @@ func (m *DockerUtility) Commands(c *typbuildtool.Context) []*cli.Command {
 	}
 }
 
-func (m *DockerUtility) dockerCompose() (root *Recipe) {
+func (m *DockerUtility) compose() (err error) {
+	if len(m.composers) < 1 {
+		return errors.New("Nothing to compose")
+	}
+	var out []byte
+	if out, err = yaml.Marshal(m.recipe()); err != nil {
+		return
+	}
+	if err = ioutil.WriteFile(dockerComposeFile, out, 0644); err != nil {
+		return
+	}
+	return
+}
+
+func (m *DockerUtility) up(c *typbuildtool.BuildContext) (err error) {
+	cmd := exec.CommandContext(c.Cli.Context, "docker-compose", "up", "--remove-orphans", "-d")
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	return cmd.Run()
+}
+
+func (m *DockerUtility) down(c *typbuildtool.BuildContext) (err error) {
+	cmd := exec.CommandContext(c.Cli.Context, "docker-compose", "down")
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	return cmd.Run()
+}
+
+func (m *DockerUtility) wipe(c *typbuildtool.BuildContext) (err error) {
+	var (
+		builder strings.Builder
+		ctx     = c.Cli.Context
+	)
+	cmd := exec.CommandContext(ctx, "docker", "ps", "-q")
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = &builder
+	if err = cmd.Run(); err != nil {
+		return
+	}
+	dockerIDs := strings.Split(builder.String(), "\n")
+	for _, id := range dockerIDs {
+		if id != "" {
+			cmd := exec.CommandContext(ctx, "docker", "kill", id)
+			cmd.Stderr = os.Stderr
+			if err = cmd.Run(); err != nil {
+				return
+			}
+		}
+	}
+	return
+}
+
+func (m *DockerUtility) recipe() (root *Recipe) {
 	root = &Recipe{
 		Version:  m.version,
 		Services: make(Services),
