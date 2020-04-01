fe783346a2854922d7e43f05fc9f7fa42880037f
iman tung <iman.tung@gmail.com>
2020-04-01 19:30:19 +0700

    add availableCommand helper to check whether the command is available

---
+++ pkg/buildkit/helper_test.go
@@ -0,0 +1,26 @@
+package buildkit_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/typical-go/typical-go/pkg/buildkit"
+)
+
+func TestCommandExist(t *testing.T) {
+	testcases := []struct {
+		name     string
+		expected bool
+	}{
+		{"cd", true},
+		{"bash", true},
+		{"", false},
+		{"invalid-command", false},
+	}
+
+	ctx := context.Background()
+	for _, tt := range testcases {
+		require.Equal(t, tt.expected, buildkit.AvailableCommand(ctx, tt.name))
+	}
+}
