3993caae4c78bbea6270f3f71212733cb1782c16
iman tung <iman.tung@gmail.com>
2020-06-09 17:17:17 +0700

    revision: fix test example in descriptor

--- typical/descriptor.go
+++ typical/descriptor.go
@@ -1,8 +1,12 @@
 package typical
 
 import (
+	"os"
+
+	"github.com/typical-go/typical-go/pkg/buildkit"
 	"github.com/typical-go/typical-go/pkg/typgo"
 	"github.com/typical-go/typical-go/wrapper"
+	"github.com/urfave/cli/v2"
 )
 
 // Descriptor of typical-go
@@ -19,5 +23,20 @@ var Descriptor = typgo.Descriptor{
 	Clean:   &typgo.StdClean{},
 	Release: &typgo.Github{Owner: "typical-go", RepoName: "typical-go"},
 
-	Utility: typgo.NewUtility(taskExamples), // Test all the examples
+	Utility: typgo.CreateUtility(&cli.Command{
+		Name:    "test-example",
+		Aliases: []string{"e"},
+		Usage:   "Test all example",
+		Action: func(cliCtx *cli.Context) (err error) {
+			gotest := &buildkit.GoTest{
+				Targets: []string{"./examples/..."},
+			}
+
+			cmd := gotest.Command()
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+
+			return cmd.Run(cliCtx.Context)
+		},
+	}), // Test all the examples
 }
