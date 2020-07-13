9001ed20d5ebe62246b5e291cd9ebe3358e40ccc
iman tung <iman.tung@gmail.com>
2020-07-14 05:48:00 +0700

    remove description in descriptor as not longer used

--- pkg/typgo/descriptor.go
+++ pkg/typgo/descriptor.go
@@ -11,8 +11,6 @@ type Descriptor struct {
 	// Name of the project (OPTIONAL). It should be a characters with/without underscore or dash.
 	// By default, project name is same with project folder
 	Name string
-	// Description of the project (OPTIONAL).
-	Description string
 	// Version of the project (OPTIONAL). By default it is 0.0.1
 	Version  string
 	Layouts  []string
