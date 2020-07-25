3d2447188393732835727387aefeccbddf5830e7
iman tung <iman.tung@gmail.com>
2020-07-25 20:29:49 +0700

    bump to version 0.10.1

--- tools/typical-build/typical-build.go
+++ tools/typical-build/typical-build.go
@@ -9,7 +9,7 @@ import (
 var (
 	descriptor = typgo.Descriptor{
 		Name:    "typical-go",
-		Version: "0.10.0",
+		Version: "0.10.1",
 		Layouts: []string{"internal", "pkg"},
 
 		Cmds: []typgo.Cmd{
