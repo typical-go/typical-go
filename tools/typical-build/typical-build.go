b72129bb1c8f93db7566237a72c835d2cb1b669b
iman tung <iman.tung@gmail.com>
2020-09-22 14:27:42 +0700

    bump to version 0.10.10

--- tools/typical-build/typical-build.go
+++ tools/typical-build/typical-build.go
@@ -10,7 +10,7 @@ var mainPkg = "."
 
 var descriptor = typgo.Descriptor{
 	ProjectName:    "typical-go",
-	ProjectVersion: "0.10.9",
+	ProjectVersion: "0.10.10",
 	ProjectLayouts: []string{"internal", "pkg"},
 
 	Cmds: []typgo.Cmd{
